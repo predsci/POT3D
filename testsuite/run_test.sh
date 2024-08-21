0abd6b037fa9083c592b802524fc971cb44eec4c
Ronald Caplan <sumseq@gmail.com>
2024-08-21 11:19:07 -0700

    Made testsuite script nicer.

--- testsuite/run_test.sh
+++ testsuite/run_test.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+    echo "Warning!  You should supply the test name and the number of MPI ranks as arguments:"
+    echo "Example:  ./run_test validation 1"
+    echo ""
+    echo "==> Setting test name to 'validation'"
+    echo ""
+    TEST="validation"
+else
+    TEST="$1"
+fi
+
+if [ -z "$2" ]
+then
+    echo "Warning!  You should supply the test name and the number of MPI ranks as arguments:"
+    echo "Example:  ./run_test validation 1" 
+    echo "" 
+    echo "==> Setting number of MPI ranks to 1"
+    echo ""
+    NP="1"
+else
+    NP="$2"
+fi
+
 POT3D_HOME=${PWD}/..
-TEST="$1"
-NP="$2"
 
 cp ${POT3D_HOME}/testsuite/${TEST}/input/* ${POT3D_HOME}/testsuite/${TEST}/run/
 cd ${POT3D_HOME}/testsuite/${TEST}/run
 
-echo "Running POT3D with $NP MPI rank..."
+echo "Running POT3D for test $TEST with $NP MPI rank(s)..."
 mpiexec -np $NP ${POT3D_HOME}/bin/pot3d > pot3d.log
 echo "Done!"
 
