de97330684998aac8fa3a099fe5660eb18a2ba0a
Ronald Caplan <sumseq@gmail.com>
2026-06-15 12:11:40 -0700

    Added launch script in bin folder for stdpar branch

---
+++ bin/launch_code_one_gpu_per_local_mpi_rank.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# ==============================================
+# Launcher for runs that use 1 GPU per MPI rank. 
+# For use with applications that do not select the device 
+# per local MPI rank within the code, such as 
+# standard langauge parallel codes on GPUs.
+# ==============================================
+
+# === LOCAL RANK SELECTION ===
+# === Uncomment the correct LOCAL_RANK 
+# === source for your environment:
+
+LOCAL_RANK=${SLURM_LOCALID}                 # Slurm
+# LOCAL_RANK=${PBS_VNODENUM}                # PBS / Torque
+# LOCAL_RANK=${OMPI_COMM_WORLD_LOCAL_RANK}  # OpenMPI
+# LOCAL_RANK=${PMI_LOCAL_RANK}              # MPICH / Intel MPI / Hydra
+
+# === If nothing was set, default to 0:
+LOCAL_RANK=${LOCAL_RANK:-0}
+
+# === Make the local MPI rank only see the GPU it should use ===
+export CUDA_VISIBLE_DEVICES=${LOCAL_RANK}                # NVIDIA
+export ROCR_VISIBLE_DEVICES=${LOCAL_RANK}                # AMD ROCm
+export HIP_VISIBLE_DEVICES=${LOCAL_RANK}                 # AMD HIP
+export ONEAPI_DEVICE_SELECTOR=level_zero:${LOCAL_RANK}   # Intel oneAPI / SYCL
+export ZE_AFFINITY_MASK=${LOCAL_RANK}                    # Intel Level Zero
+
+# === Fancy launch: use numactl to bind the cpu and memory 
+# === location (not usable for all configs)
+#exec numactl --cpunodebind=${LOCAL_RANK} \
+#             --membind=${LOCAL_RANK} \
+#             "$@"
+
+# === Basic launch ===
+exec "$@"
+
+
+
